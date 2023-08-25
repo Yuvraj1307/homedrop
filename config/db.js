aa06a04aa2b127b714b45394f64e8183ce6f1c92
yuvraj maharshi <112754852+Yuvraj1307@users.noreply.github.com>
2023-08-25 19:00:31 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose")
-const connection=mongoose.connect("mongodb+srv://yuvraj:yuvraj@cluster0.hhjiny0.mongodb.net/homedrop?retryWrites=true&w=majority")
-async function makeConnect(){
-    await connection
-    console.log("connected to DB")
-}
-module.exports={makeConnect}
